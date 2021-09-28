c3751b7bd02a4638cb1a1870f9549539b5ed850f
Clara <charmonson11@gmail.com>
2021-09-28 18:17:13 -0500

    test

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,18 @@
-exports.perimeter = (x,y)=> 2* (x+y);
-exports.area= (x,y) => (x*y);
+module.exports = (x,y,callback)=>{ /*its common practice to do error handling w callback function*/
+    if (x <= 0 || y <= 0){
+        callback(new Error(`Rectangle dimensions must be greater 
+        than zero. Received: ${x}, ${y}`))
+    } else {
+        setTimeout (() =>
+            callback(null, {
+                perimeter: (x,y)=> 2* (x+y),
+                area: (x,y) => (x*y)
+            }),
+            2000
+        );
+    }
+};
+
+
+
+
