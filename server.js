1b001d9898f0b5da62fdd01096ddb0ded57a6751
Charmonson <77593573+Charmonson@users.noreply.github.com>
2021-10-01 08:25:04 -0500

    node http example 2

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-const express = require('express');
+const http = require('http');
+
+const path = require('path');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const app = express(); //returns express server app 
+const server = http.createServer((req, res) => {
+    console.log (`Request for ${req.url} by method ${req.method}`);
 
-app.use((req, res) => { //return same response for any request 
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+    if (req.method === 'GET') {
+
+        let fileUrl = req.url;
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt === '.html') {
+                fs.access(filePath, err => {
+                if (err) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+
+                fs.createReadStream(filePath).pipe(res);
+            });
+                 
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+        }
+
+
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
+
 });
 
-app.listen(port, hostname, () => { // creates an instance of the http server class
-    console.log(`Server running at http://${hostname}:${port}/`); // and starts listening to it
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
