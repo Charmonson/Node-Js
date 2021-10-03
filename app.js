3b983fb64b20a596e1db3d7c484c55de98086000
Charmonson <77593573+Charmonson@users.noreply.github.com>
2021-10-03 17:30:28 -0500

    Express Generator

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const rect = require('./rectangle');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-function solveRect(l, w){
-    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-    rect(l,w, (err, rectangle) => {
+var app = express();
 
-        if (err) {
-            console.log('ERROR:',err.message);
-    }
-    else {
-        console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
-        console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
-    }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-    console.log('This statement is loged after the call to rect()');
-}
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-solveRect (2,4);
-solveRect(3,5);
-solveRect(-1,2);
-solveRect(4,0);
+module.exports = app;
