ae1508f2cccdbcbdad6f4c6ea20c2259554696f2
Charmonson <77593573+Charmonson@users.noreply.github.com>
2021-10-09 15:32:23 -0500

    mongoose part 2

--- index.js
+++ index.js
@@ -1,48 +1,34 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert').strict;
-const dboper = require('./operations');
+const mongoose = require('mongoose');
+const Campsite = require('./models/campsite');
 
-const url = 'mongodb://localhost:27017/';
-const dbname = 'nucampsite';
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-
-    assert.strictEqual(err, null);
+connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    const db = client.db(dbname);
-
-    db.dropCollection('campsites', (err, result) => {
-        assert.strictEqual(err, null);
-        console.log('Dropped Collection:', result);
-
-        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
-            'campsites', result => {
-            console.log('Insert Document:', result.ops);
-
-            dboper.findDocuments(db, 'campsites', docs => {
-                console.log('Found Documents:', docs);
-
-                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
-                    { description: "Updated Test Description" }, 'campsites',
-                    result => {
-                        console.log('Updated Document Count:', result.result.nModified);
-
-                        dboper.findDocuments(db, 'campsites', docs => {
-                            console.log('Found Documents:', docs);
-                            
-                            dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
-                                'campsites', result => {
-                                    console.log('Deleted Document Count:', result.deletedCount);
-
-                                    client.close();
-                                }
-                            );
-                        });
-                    }
-                );
-            });
-        });
+    Campsite.create({
+        name: 'React Lake Campground',
+        description: 'test'
+    })
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.find();
+    })
+    .then(campsites => {
+        console.log(campsites);
+        return Campsite.deleteMany();
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch(err => {
+        console.log(err);
+        mongoose.connection.close();
     });
 });
